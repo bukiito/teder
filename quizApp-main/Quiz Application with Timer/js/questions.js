faf19b1ca5222ac428cc3cff998203da0c3dfffc
bukiito <86784109+bukiito@users.noreply.github.com>
2022-11-29 11:27:35 +0300

    Add files via upload

---
+++ quizApp-main/Quiz Application with Timer/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was Sunye born?",
+    answer: "12.08.1989",
+    options: [
+      "02.05.1989",
+      "04.10.1989",
+      "26.05.1989",
+      "12.08.1989"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the name of Sunye's solo album?",
+    answer: "Genuine",
+    options: [
+      "First Page",
+      "Reflection of Christ",
+      "Genuine",
+      "Glass Heart"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When will Sunye's album be released?",
+    answer: "26.07.2022",
+    options: [
+      "26.07.2022",
+      "23.07.2022",
+      "20.07.2022",
+      "29.07.2022"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which group did Sunye debut as a member of in 2022?",
+    answer: "MAMADOL",
+    options: [
+      "MAMIDOL",
+      "MOMIDOL",
+      "MAMADOL",
+      "MAAMDOL"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What song did Sunye perform with Davichi and Taeyeon at SBS Gayo Daejun 2008?",
+    answer: "Stand Up For Love",
+    options: [
+      "U—Go Girl",
+      "Love Like Oxygen",
+      "Flowerpot",
+      "Stand Up For Love"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
